248a082e8673704f6a2148f9a05ba81c53454e27
Andrew Garcia <andrewagarcia8@gmail.com>
2022-02-20 08:44:42 -0800

    Working on writefile

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const fs = require('fs');
 
 // Add Markdown page to index.js
 const generateReadMe = require('./utils/generateMarkdown.js');
+
 // TODO: Create an array of questions for user input
 const questions = () => {
     return inquirer.prompt([
@@ -56,51 +57,19 @@ const questions = () => {
     ]);
 };
 
-const generateHTML = ({ name, location, github, linkedin }) =>
-  `<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-  <title>Document</title>
-</head>
-<body>
-  <div class="jumbotron jumbotron-fluid">
-  <div class="container">
-    <h1 class="display-4">Hi! My name is ${name}</h1>
-    <p class="lead">I am from ${location}.</p>
-    <h3>Example heading <span class="badge badge-secondary">Contact Me</span></h3>
-    <ul class="list-group">
-      <li class="list-group-item">My GitHub username is ${github}</li>
-      <li class="list-group-item">LinkedIn: ${linkedin}</li>
-    </ul>
-  </div>
-</div>
-</body>
-</html>`;
 
-// Bonus using writeFileSync as a promise
-const init = () => {
-  questions()
-    // Use writeFileSync method to use promises instead of a callback function
-    .then((answers) => fs.writeFileSync('index.html', generateHTML(answers)))
-    .then(() => console.log('Successfully wrote to index.html'))
-    .catch((err) => console.error(err));
-};
 
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err)=> {
+        if (err) {
+            console.log(err)
+        }
+        console.log('Your README has been generated');
+    });
+}
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
 init();
-
-
-
-
-
-
-// // TODO: Create a function to write README file
-// function writeToFile(fileName, data) {}
-
-// // TODO: Create a function to initialize app
-// function init() {}
-
-// // Function call to initialize app
-// init();
